873b8121d8a2dfb5320478579189426ae771e948
Marek Mościchowski <marek.moscichowski@miquido.com>
2026-04-20 13:02:37 +0200

    1.0.0

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "random_password" "dynamic_user" {
 }
 
 module "docker_host" {
-  source = "git::https://github.com/miquido/terraform-docker-host.git"
+  source = "../docker-host"
 
   domain                      = var.domain
   acme_email                  = var.acme_email
